57d9b838d114fbea53d50b5c97c0612242ffc16c
RohaanMooken <rohaanmooken@gmail.com>
2023-08-29 09:39:18 +0200

    Update rohaanSolve.py

--- Project-Euler/MultiplesOf3Or5/rohaanSolve.py
+++ Project-Euler/MultiplesOf3Or5/rohaanSolve.py
@@ -1,16 +1,10 @@
 sum = 0
-iteration = 0
-term = 0
 
-for i in range(3, 6, 2):
-    term = 0
-    iteration = 0
-    while term < 1000:
-        term = i * iteration
-        if term >= 1000:
-            continue
-        # print(f"{i}\t{iteration}\t{term}")
-        sum += term
-        iteration += 1
+for i in range(0, 1000, 3):
+    sum += i
+
+for i in range(0,1000, 5):
+    if i % 3 != 0:
+        sum += i
 
 print(sum)
