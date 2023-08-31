8a3933da9ca0a2194db1da6ca9fe90fc8eb5025b
Tobias Samsonsen <Tobias.samsonsen@gmail.com>
2023-08-31 10:22:25 +0200

    MORE OPPGAVER!!!!

--- Grunnleggende-Programmering/Løkker/Løkker-Med-En-Variabel/Oppgave 1.3.1.4/tobiasSolve.py
+++ Grunnleggende-Programmering/Løkker/Løkker-Med-En-Variabel/Oppgave 1.3.1.4/tobiasSolve.py
@@ -2,3 +2,27 @@
 
 for i in range(1, 16):
     print(i, end=" ")
+    
+for i in range(1, 16):
+    print(i)
+    
+for i in range(1, 16, 2):
+    print(i, end=" ")
+    
+    
+# While-løkker
+
+i = 1
+while i < 16:
+    print(i, end=" ")
+    i += 1
+    
+i = 1
+while i < 16:
+    print(i)
+    i += 1
+    
+i = 1
+while i < 16:
+    print(i, end=" ")
+    i += 2
