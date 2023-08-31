51456f417b038389a6aa1e88a6b32dd00c680ede
asdadof <Sebnorlar@gmail.com>
2023-08-31 10:12:22 +0200

    Update sebastianSolve.py

--- Grunnleggende-Programmering/Løkker/Nøstede-Løkker/Oppgave 1.3.3.5/sebastianSolve.py
+++ Grunnleggende-Programmering/Løkker/Nøstede-Løkker/Oppgave 1.3.3.5/sebastianSolve.py
@@ -7,8 +7,36 @@ Trekanttall nummer n er summen av alle positive heltall fra og med 1 til og med 
 Skriv et program som spør brukeren om hvilket tall hen ønsker, og bruker én løkke til å beregne tallet. Skriv ut tallet.
 '''
 
+#A)
 tall = int(input("Tall: "))
 x = 0
 for i in range(1,tall+1):
     x +=i
 print(x)
+
+
+'''
+Et tetraeder er en pyramide der alle sidene er likesidede trekanter.
+
+B:
+Tetraedertall (~pyramidetall) nummer n er summen av alle trekanttall fra og med nummer 1 til og med nummer n.
+
+Skriv et program som spør brukeren om hvilket tall hen ønsker, og bruker nøstede løkker til å beregne tallet.
+Skriv ut tallet.
+''' 
+
+#B) 
+
+tall = int(input("Tall: "))
+trekanttall = 0
+pyramidetall = 0 
+for i in range(1,tall+1):
+    trekanttall += i
+    
+    
+    pyramidetall += trekanttall
+
+print(pyramidetall)  
+
+
+
