06a0fd8b726a66d8ce1c8600fcdd6bbe2cf1ed47
asdadof <Sebnorlar@gmail.com>
2023-08-31 10:13:25 +0200

    -

--- Grunnleggende-Programmering/Løkker/Nøstede-Løkker/Oppgave 1.3.3.5/sebastianSolveA.py
+++ Grunnleggende-Programmering/Løkker/Nøstede-Løkker/Oppgave 1.3.3.5/sebastianSolveA.py
@@ -13,30 +13,3 @@ x = 0
 for i in range(1,tall+1):
     x +=i
 print(x)
-
-
-'''
-Et tetraeder er en pyramide der alle sidene er likesidede trekanter.
-
-B:
-Tetraedertall (~pyramidetall) nummer n er summen av alle trekanttall fra og med nummer 1 til og med nummer n.
-
-Skriv et program som spør brukeren om hvilket tall hen ønsker, og bruker nøstede løkker til å beregne tallet.
-Skriv ut tallet.
-''' 
-
-#B) 
-
-tall = int(input("Tall: "))
-trekanttall = 0
-pyramidetall = 0 
-for i in range(1,tall+1):
-    trekanttall += i
-    
-    
-    pyramidetall += trekanttall
-
-print(pyramidetall)  
-
-
-
