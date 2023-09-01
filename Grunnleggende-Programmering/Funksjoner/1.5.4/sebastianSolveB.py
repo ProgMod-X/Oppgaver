bd0538c0d1b8ad96f2f89e0d90f9659ff4694599
asdadof <Sebnorlar@gmail.com>
2023-09-01 20:05:10 +0200

    Create sebastianSolveB.py

---
+++ Grunnleggende-Programmering/Funksjoner/1.5.4/sebastianSolveB.py
@@ -0,0 +1,10 @@
+'''
+
+Funksjonen tar tre parametre (mengde i gram) og returnerer én verdi – energi i kilojoule (kJ)
+
+Lag et program der brukeren kan oppgi mengde protein, fett og karbohydrater, og få utregnet energimengden i maten. Bruk funksjonen du laget over
+'''
+
+
+def kilojoule(P,F,C):
+    return 17*P + 38*F + 17*C
