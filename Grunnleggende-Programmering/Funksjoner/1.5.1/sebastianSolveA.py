e67d29083429ce812a7a159f550922d6132cef8f
asdadof <Sebnorlar@gmail.com>
2023-09-01 18:36:04 +0200

    .... . .., ... .- -. -.

--- Grunnleggende-Programmering/Funksjoner/1.5.1/sebastianSolveA.py
+++ Grunnleggende-Programmering/Funksjoner/1.5.1/sebastianSolveA.py
@@ -1,7 +1,5 @@
 '''
 Lag en funksjon som krever én parameter, og returnerer verdi etter følgende matematiske funksjon: f(x) = x⋅x + 3 .
-
-Lag et program som skriver ut verdiene av f(x)  for x mellom 1 og 15 . Programmet skal bruke en løkke og funksjonen du laget over.
 '''
 x = int(input("X = ")) 
 
