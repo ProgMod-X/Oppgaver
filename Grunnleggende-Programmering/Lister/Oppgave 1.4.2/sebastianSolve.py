27ec815ea716259cbb5c375a31098c3fba57eeeb
asdadof <Sebnorlar@gmail.com>
2023-09-01 14:57:52 +0200

    Create sebastianSolve.py

---
+++ Grunnleggende-Programmering/Lister/Oppgave 1.4.2/sebastianSolve.py
@@ -0,0 +1,35 @@
+'''
+Lag et program som fyller en liste med tilfeldige tall mellom fra og med 1 til og med 100.
+
+Programmet skal deretter
+
+1: Beregne gjennomsnittet
+2: Finne største og minste verdi
+
+
+Eksempel på utskrift:
+
+```bash
+Values: 86, 81, 17, 62, 36, 13, 79, 32, 46, 60
+Mean:   51.2
+Min:    13
+Max:    86
+```
+'''
+import random 
+import statistics
+
+x = []
+
+y = 100
+
+for i in range(y):
+    x.append(random.randint(1,100))
+
+
+a = min(x)
+b = max(x)
+c = statistics.mean(x)
+
+print(f"Verider: {x}\n Min: {a}\n Max: {b}\n Gjennomsnitt: {c}")
+
