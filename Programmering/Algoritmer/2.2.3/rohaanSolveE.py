8af0f9ace1aeac34018d95944ceaf53014b3e8a1
RohaanMooken <rohaanmooken@gmail.com>
2023-09-07 12:23:27 +0200

    2.2.3 E

---
+++ Programmering/Algoritmer/2.2.3/rohaanSolveE.py
@@ -0,0 +1,49 @@
+import random
+
+operators = ["+", "-", "*", "/"]
+operator = int(input("Hvilken vil du ha?\n +(0), -(1), *(2), /(3), blanding(4): "))
+difficulty = int(input("Hvilken vanskelighets grad?\nEnkel(0), Middels(1), Vanskelig(2): "))
+n = int(input("Hvor mange spørsmål? "))
+
+if difficulty == 0:
+    difficulty = 10
+elif difficulty == 1:
+    difficulty = 25
+elif difficulty == 2:
+    difficulty = 100
+
+
+def game(operator, numbers):
+
+    correct = 0
+    for i in range(numbers):
+        if operator == 4:
+            opr = random.choice(operators)
+        else:
+            opr = operators[operator]
+
+        a = random.randint(1, difficulty)
+        b = random.randint(1, difficulty)
+        c = float(input(f"Hva er {a} {opr} {b}: "))
+
+        if (c == a + b and operator == 0) or operator == 4:
+            correct += 1
+        elif (c == a - b and operator == 1) or operator == 4:
+            correct += 1
+        elif (c == a * b and operator == 2) or operator == 4:
+            correct += 1
+        elif (c == round(a / b, 2) and operator == 3) or operator == 4:
+            correct += 1
+
+    return correct
+
+correct = game(operator, n)
+
+if correct == n:
+    print("Perfekt")
+elif correct > n / 3 * 2:
+    print("Bra!")
+elif correct <= n / 3 * 2 and correct >= n / 3:
+    print("Du trenger mer trening...")
+else:
+    print("Spør mattelæreren din om hjelp!")
