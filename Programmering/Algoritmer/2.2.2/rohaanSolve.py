2d84428777491952ab3907c4d56c55870a991795
RohaanMooken <rohaanmooken@gmail.com>
2023-09-07 09:35:52 +0200

    2.2.2 partial

---
+++ Programmering/Algoritmer/2.2.2/rohaanSolve.py
@@ -0,0 +1,15 @@
+def square(x):
+    for i in range(x):
+        for j in range(x):
+            print("*")
+        print("\n")
+
+
+def rectangle(x):
+    pass
+
+
+def triangle(x):
+    pass
+
+square(3)
