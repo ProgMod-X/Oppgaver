3bb67d519ca8cca80869969bc99d21730527229c
asdadof <Sebnorlar@gmail.com>
2023-09-12 17:43:56 +0200

    Create sebastiansolve.py

---
+++ Programmering/Småprogrammer/2.2.2/sebastiansolve.py
@@ -0,0 +1,34 @@
+
+def square(a):
+    for x in range(a):
+        print("*"*a)
+        "\n"
+    return print()
+
+square(5)
+
+def rectangle(b,c):
+    for x in range(b):
+        print("*"*c)
+        "\n"
+    return print()
+
+rectangle(5,6)
+
+
+
+def rtriangle(d):
+    x = 0
+    while x <= d:
+        print("*"*x)
+        x += 1
+        
+rtriangle(5)
+
+
+def ltriangle(d):
+    (d-1)/2
+    return print()    
+ltriangle(5)
+
+
