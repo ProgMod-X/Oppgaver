d8e75e30eb640af393ef9ddcf5e3e0d3e3c0cccb
Tobias Samsonsen <Tobias.samsonsen@gmail.com>
2023-09-12 13:24:52 +0200

    Oppgaver + typo fix Rohaan *cough*

--- Programmering/Småprogrammer/2.2.2/tobiasSolve.py
+++ Programmering/Småprogrammer/2.2.2/tobiasSolve.py
@@ -3,19 +3,23 @@ def square(n):
         for j in range(n):
             print("*", end="")
         print("\n")
+    print("---------------------------")
         
 def rectangle(x, y):
     for i in range(y):
         for j in range(x):
             print("*", end="")
         print("\n")
+    print("---------------------------")
         
 def triangle(n):
     for i in range(n + 1):
         for j in range(i):
             print("*", end="")
         print("\n")
-        
-square(3)
-rectangle(4,2)
-triangle(6)
+    print("---------------------------")
+
+    
+square(int(input("Square size: ")))
+rectangle(int(input("Rectangle x: ")), int(input("Rectangle y: ")))
+triangle(int(input("Triangle size: ")))
