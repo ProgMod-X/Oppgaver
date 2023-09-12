3b0478ff250e652ca44b169d780d6f7490fdcfbd
asdadof <Sebnorlar@gmail.com>
2023-09-12 18:47:22 +0200

    Create sebastianSolve.py

---
+++ Programmering/Småprogrammer/2.2.3/sebastianSolve.py
@@ -0,0 +1,17 @@
+#A)
+'''
+import random as rd 
+
+
+x,y = rd.randint(1,10),rd.randint(1,10)
+
+svar = int(input(f"Hva er {x} * {y}? "))
+
+if svar == x*y:
+    print("Riktig!")
+else: 
+    print("Feil")
+'''
+
+#B) 
+
