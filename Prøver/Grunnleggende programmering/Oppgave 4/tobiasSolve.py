9d407afae333d8a02973f6359bdf206a7ed47d54
Tobias Samsonsen <Tobias.samsonsen@gmail.com>
2023-09-28 09:49:56 +0200

    oppdatering

--- Prøver/Grunnleggende programmering/Oppgave 4/tobiasSolve.py
+++ Prøver/Grunnleggende programmering/Oppgave 4/tobiasSolve.py
@@ -5,11 +5,11 @@ for i in range(7):
     
 myNumbers[(len(myNumbers)//2)] = 42
 
-numbers = [2,355,6,34,34,56,3,4,534,6,18] # Ukjent liste
+numbers = [18,355,6,34,34,56,3,4,534,6,3] # Ukjent liste
 
 myNumbers[0] = numbers[-1]
 
 if numbers[0] > 17:
-    myNumbers[(len(myNumbers)-1)] = numbers[0] # Blir lagt inn på feil plass av en eller annen grunn
+    myNumbers[(len(myNumbers)-1)] = numbers[0]
     
 print(myNumbers)
