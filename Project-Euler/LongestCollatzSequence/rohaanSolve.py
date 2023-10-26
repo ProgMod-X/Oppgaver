756f2bb0f161909d0368f6354ed9ec77f9b80230
RohaanMooken <rohaanmooken@gmail.com>
2023-10-26 10:01:25 +0200

    Update rohaanSolve.py

--- Project-Euler/LongestCollatzSequence/rohaanSolve.py
+++ Project-Euler/LongestCollatzSequence/rohaanSolve.py
@@ -5,22 +5,23 @@ longest = (0, 0)
 
 def f(n):
     global count
-    count += 1
 
     if n in mem:
-        return mem[n]
+        count += mem[n]
+        return
     elif n % 2 == 0:
+        count += 1
         a = f(n / 2)
-        mem[n] = a
         return a
     else:
+        count += 1
         a = f(3 * n + 1)
-        mem[n] = a
         return a
-    
+
 for i in range(1, 1000000):
     count = 0
     f(i)
     if count > longest[1]:
         longest = (i, count)
-    print(f"{i}\t{longest[0]}\t{longest[1]}")
+        mem[i] = count
+    print(f"{i}\t{count}\t{longest[0]}\t{longest[1]}\t{len(mem)}")
