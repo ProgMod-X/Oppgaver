811f0d9598689417fcbed1bee2d8d269a643f38c
Tobias Samsonsen <Tobias.samsonsen@gmail.com>
2023-11-13 11:28:13 +0100

    Tobias Fast

--- Project-Euler/SmallestMultiple/tobiasSolve.py
+++ Project-Euler/SmallestMultiple/tobiasSolve.py
@@ -1,10 +1,14 @@
+import time
+
 evenlyDivisible = False
 num = 20
 longestDivisible = []
 
+start = time.time()
+
 while evenlyDivisible == False:
     divisibleWith = []
-    for i in range(1, 21):
+    for i in range(1, 20):
         if num % i == 0:
             divisibleWith.append(i)
         else:
@@ -12,6 +16,10 @@ while evenlyDivisible == False:
     if len(divisibleWith) > len(longestDivisible):
         print(divisibleWith, num)
         longestDivisible = divisibleWith
-    if len(divisibleWith) == 20:
+    if len(divisibleWith) == 19:
         evenlyDivisible = True
     num += 20
+    
+end = time.time()
+
+print(end-start)
