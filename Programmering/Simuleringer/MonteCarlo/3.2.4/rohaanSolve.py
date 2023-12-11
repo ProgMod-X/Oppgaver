91dc57a9da1b6295b2967cb536f6c2da433209d3
RohaanMooken <rohaanmooken@gmail.com>
2023-12-11 12:45:15 +0100

    3.2.4

---
+++ Programmering/Simuleringer/MonteCarlo/3.2.4/rohaanSolve.py
@@ -0,0 +1,18 @@
+import random as rnd
+counter, width, height = 0, 5, 6
+N = 10000000
+for i in range(N):
+    m1 = (rnd.random() * width, 0)
+    m2 = (rnd.random() * width, height)
+    p1 = (rnd.random() * width, rnd.random() * height)
+    p2 = (rnd.random() * width, rnd.random() * height)
+
+    sm = (m2[1]  - m1[1]) / (m2[0] - m1[0])
+    sp = (p2[1]  - p1[1]) / (p2[0] - p1[0])
+
+    if (p1[0] * sp < p1[0] * sm and p2[0] * sp > p2[0] * sm):
+        counter += 1
+    elif (p1[0] * sp > p1[0] * sm and p2[0] * sp < p2[0] * sm):
+        counter += 1
+
+print(f"{counter / N * 100}%")
