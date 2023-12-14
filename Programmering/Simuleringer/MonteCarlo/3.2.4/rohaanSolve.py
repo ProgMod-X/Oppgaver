2c97ef9dd6fc29aaa35d5d6775f04771cee8a090
RohaanMooken <rohaanmooken@gmail.com>
2023-12-14 09:05:47 +0100

    2.3.x

--- Programmering/Simuleringer/MonteCarlo/3.2.4/rohaanSolve.py
+++ Programmering/Simuleringer/MonteCarlo/3.2.4/rohaanSolve.py
@@ -10,9 +10,13 @@ for i in range(N):
     sm = (m2[1]  - m1[1]) / (m2[0] - m1[0])
     sp = (p2[1]  - p1[1]) / (p2[0] - p1[0])
 
-    if (p1[0] * sp < p1[0] * sm and p2[0] * sp > p2[0] * sm):
+    if (p1[0] * sp < p1[0] * sm) and (p2[0] * sp > p2[0] * sm):
         counter += 1
-    elif (p1[0] * sp > p1[0] * sm and p2[0] * sp < p2[0] * sm):
+    elif (p1[0] * sp > p1[0] * sm) and (p2[0] * sp < p2[0] * sm):
         counter += 1
 
+    # print((p1[0] * sp), ( p1[0] * sm), (p2[0] * sp) , p2[0] * sm)
+    print(sm, sp)
+
+print(counter)
 print(f"{counter / N * 100}%")
