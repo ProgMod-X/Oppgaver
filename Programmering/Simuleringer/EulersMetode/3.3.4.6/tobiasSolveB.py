6b59d666b22eb1afd16a40aa4e1ffc4aeb529731
Tobias Samsonsen <Tobias.samsonsen@gmail.com>
2024-01-23 12:58:32 +0100

    nyooooom

--- Programmering/Simuleringer/EulersMetode/3.3.4.6/tobiasSolveB.py
+++ Programmering/Simuleringer/EulersMetode/3.3.4.6/tobiasSolveB.py
@@ -3,13 +3,13 @@ def deltaY(k, y, w):
 
 factor = -0.25
 change = 20
-inBody = 0
+inBody = 80
 
 while inBody > 60:
     inBody = 0
-    change -= 0.01
-    for i in range(1000):
+    change -= 0.0001
+    for i in range(100):
         inBody += deltaY(factor, inBody, change)
 
     
-print(f"Hun må ta tabletter på {}")
+print(f"Hun må ta tabletter på litt mindre enn {round(change)} mg for å ikke få for mye i seg")
