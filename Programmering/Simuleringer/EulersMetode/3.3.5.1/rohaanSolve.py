07c03c1e67ae783cfdbb8d64c82fa1ac47dae411
RohaanMooken <rohaanmooken@gmail.com>
2024-01-23 13:49:07 +0100

    3.3.5.1

--- Programmering/Simuleringer/EulersMetode/3.3.5.1/rohaanSolve.py
+++ Programmering/Simuleringer/EulersMetode/3.3.5.1/rohaanSolve.py
@@ -14,6 +14,8 @@ while x < 3000:
     if int(x) % 50 == 0:
         x_list.append(x)
         y_list.append(tank)
+    if round(tank, 3) == 200:
+        print(f"Det må tappes ut {x} liter diesel før halvparten er biodiesel")
     tank += (1 - tank * 0.0025) * dx
     x += dx
 
