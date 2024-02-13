d0cd7eebfaa0b9ec1d751f700af86826c90703c6
Tobias Samsonsen <Tobias.samsonsen@gmail.com>
2024-02-13 14:36:11 +0100

    sløkdjfsd

--- Programmering/Modellering/4.0.2/tobiasSolve.py
+++ Programmering/Modellering/4.0.2/tobiasSolve.py
@@ -10,8 +10,10 @@ def S(line, x_values, y_values):
         sum += abs(line(x_values[index]) - y_values[index])**2
     return sum
 
-x_values = [1,2,3,4,5,6,7,8,9]
-y_values = [3,-3,6,24,14,24,4,1,37]
+
+# DATASET = [(1,1), (13,4), (11, 9), (4, 3), (5, 6), (8, 9)]
+x_values = [1,13,11,4,5,8]
+y_values = [1,4,9,3,6,9]
 
 # Finn balansepunkt
 xsum = 0
@@ -37,8 +39,8 @@ while stigningstall < st_slutt:
 
     stigningstall += 0.001
     
-print(f"Sum = {min(S_list)}")
-print(f"Stigningstall = {st_list[S_list.index(min(S_list))]}")
+print(f"Sum = {round(min(S_list), 3)}")
+print(f"Stigningstall = {round(st_list[S_list.index(min(S_list))], 3)}")
 
 x = np.linspace(min(x_values), max(x_values))
 
